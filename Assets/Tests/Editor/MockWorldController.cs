4a3eabed5b6ad4638c6b53300406e326a8f45e85
pierreyoda <pierreyoda@users.noreply.github.com>
2018-02-09 21:19:18 +0100

    Script : basic Unit Testing infrastructure in place

---
+++ Assets/Tests/Editor/MockWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Script;
+
+public class MockWorldController : IScriptContext {
+	private List<IFunction> functions = new List<IFunction>();
+	private List<LocalVariable> localVariables = new List<LocalVariable>();
+	private List<GlobalVariable> globalVariables = new List<GlobalVariable>();
+
+	private DateTime date = new DateTime(1980, 1, 1);
+	private Employee currentEmployee = null;
+
+	public List<IFunction> Functions() => functions;
+	public List<LocalVariable> LocalVariables() => localVariables;
+	public List<GlobalVariable> GlobalVariables() => globalVariables;
+
+	public bool SetGlobalVariable(string name, ISymbol value) {
+		throw new NotImplementedException();
+	}
+
+	public DateTime D() => date;
+	public GameDevCompany C() => null;
+
+	public Employee CurrentEmployee() => currentEmployee;
+	public void SetCurrentEmployee(Employee employee) {
+		currentEmployee = employee;
+	}
+}
---
+++ Assets/Tests/Editor/ScripSystemTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using Script;
+
+[TestFixture]
+public class ScripSystemTests {
+	private ParserContext parserContext;
+	private MockWorldController scriptContext;
+
+	[OneTimeSetUp]
+	public void ScriptSystemTestContext() {
+		scriptContext = new MockWorldController();
+		parserContext = new ParserContext {
+			Grammar = Grammar.DefaultGrammar(),
+			LocalVariables = scriptContext.LocalVariables(),
+			GlobalVariables = scriptContext.GlobalVariables(),
+			Functions = scriptContext.Functions(),
+		};
+	}
+
+	private void AssertScriptResult(ISymbol expected, string script) {
+		Executable executable = Executable.FromScript(script, parserContext);
+		ISymbol result = executable.Execute(scriptContext);
+		Assert.AreEqual(expected.Type(), result.Type());
+		Assert.AreEqual(expected.ArrayType(), result.ArrayType());
+		Assert.AreEqual(expected.ValueString(), result.ValueString());
+		scriptContext.LocalVariables().Clear();
+	}
+
+	[Test]
+	public void ScriptSystemSimpleParsing() {
+		AssertScriptResult(new IntegerSymbol(9), @"
+			let a: int = 2;
+			const b: int = 3;
+			a * 4 + b / 2
+		");
+		AssertScriptResult(new FloatSymbol(-5.5f), @"
+			const a: float = 5.0;
+			-2.0 * a + 2.0 ^ 2.0 + 3.0 / 2.0 - 0.5 * 2.0
+		");
+		AssertScriptResult(new StringSymbol("string"), @"
+			const a: string = 'str';
+			const b: string = a + 'in';
+			b + 'g'
+		");
+	}
+}
